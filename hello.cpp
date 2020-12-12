df49bcf0f2ab3ffe043ecfcaf531533e28c6af96
Takatoshi Kondo <redboltz@gmail.com>
2020-12-12 21:39:23 +0900

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+
+#include <boost/log/core.hpp>
+#include <boost/log/attributes.hpp>
+#include <boost/log/attributes/scoped_attribute.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/expressions/keyword.hpp>
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+
 int main() {
     std::cout << "cout" << std::endl;
     std::cerr << "cerr" << std::endl;
     std::clog << "clog" << std::endl;
+    BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
 }
